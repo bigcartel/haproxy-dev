5d8b987bb82bd11dab9e2140f9e7e8ce75099b29
Lee Jensen <lee@bigcartel.com>
2023-03-23 20:56:58 +0000

    fix(ssl) cert removal not working
    This isn't bash and that form of shell expansion won't fly. We need to
    remove the source files because otherwise haproxy tries to read them and
    complains that they are missing keys or certs respectively.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 if [ ! -f /var/ssl/haproxy.pem ]; then
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /var/ssl/haproxy.key -out /var/ssl/haproxy.crt -subj "/C=US/ST=UT/L=Salt Lake City/O=Big Cartel/OU=IT Department/CN=*.bigcartel.test"
     cat /var/ssl/haproxy.key /var/ssl/haproxy.crt > /var/ssl/haproxy.pem
-    rm /var/ssl/haproxy.{key,crt}
+    rm /var/ssl/haproxy.key /var/ssl/haproxy.crt
 fi
 
 exec "$@"
